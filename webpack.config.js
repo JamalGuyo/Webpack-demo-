88a2486e3a32b5ab32e8a960dc6a3e6b34488564
JamalGuyo <jamalguyo08@gmail.com>
2019-10-17 15:47:38 +0300

    create cache busting in main.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.[contenthash].js'
   },
   module: {
     rules: [
